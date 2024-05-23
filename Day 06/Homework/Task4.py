f574356c7c35cdc65042a55714491cb28b90d397
nikalobja <nikalobja2007@gmail.com>
2024-05-24 00:57:23 +0400

    commit

---
+++ Day 06/Homework/Task4.py
@@ -0,0 +1,8 @@
+"""4. პირველ რიგში, მომხმარებელს შემოატანინეთ სთრინგი და ის გაამრავლეთ ხუთზე. 
+შემდეგ შემოატანინეთ რიცხვი და ისიც გაამრავლეთ ხუთზე. ორივე შედეგი დაბეჭდეთ და კომენტარების გამოყენებით აღწერეთ თითოეული."""
+
+
+string = input("enter string: ")
+number = input("enter number: ")
+print(string * 5) # string multiplication
+print(int(number) * 5) #arithmetical operation
---
+++ Day 06/Homework/Task5.py
@@ -0,0 +1,11 @@
+"""5. მომხმარებელს შემოატანინეთ ორი რიცხვი. თქვენი დავალებაა შექმნათ კალკულატორი, დაბეჭდეთ: 
+რიცხვების ჯამი (num1 + num2), სხვაობა (num1 - num2), ნამრავლი (num1 * num2), განაყოფი (num1 / num2)"""
+
+
+num1 = int(input("enter your number: "))
+num2 = int(input("enter your number2: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
