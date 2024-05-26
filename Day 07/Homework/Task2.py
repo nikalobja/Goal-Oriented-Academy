8bde2b474091508de14974540ba4d38ce5176490
nikalobja <nikalobja2007@gmail.com>
2024-05-27 02:40:53 +0400

    commit

---
+++ Day 07/Homework/Task2.py
@@ -0,0 +1,24 @@
+"""2)შექმენით პროგრამა სადაც მომხმარებელი შემოიტანს 5 რციხვს, 
+ხოლო ამ 5 რიცხვს შორის გამოიყენეთ ყველა არითმეტიკული ოპერაცია (// და %_იც), 
+საბოლოოდ დაბეჭდეთ შედეგები ტერმინალში + ახსენით თითოეული ნაწილი კოდის რატომ დაწერეთ კონკრეტული ხაზი და რას აკეთებს."""
+
+
+number = int(input("enter your number: "))#declared varieble as integer converted input value
+number1 = int(input("enter your number1: "))
+number2 = int(input("enter your number2: "))
+number3 = int(input("enter your number3: "))
+number4 = int(input("enter your number4: "))
+print(number + number1)#
+print(number1 - number2)
+print(number4 / number3)
+print(number3 * number4)
+print(number2 // number4)
+print(number % number2)
+
+
+
+
+
+
+
+
